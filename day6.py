34e78248eb7a4ad55eab954cb4edd714cdeb1b29
Matt Luker <mluker@ostusa.com>
2020-12-06 18:08:33 -0600

    part 2

--- day6.py
+++ day6.py
@@ -1,18 +1,23 @@
-f = open("day6input.txt", "r")
 runningCount = 0
+runningCountAllYes = 0
+peopleInGroup = 0
 answers = {}
 
+f = open("day6input.txt", "r")
 for l in f:
     if l == "\n":
         runningCount += len(answers)
-        #print('group count: {}'.format(len(answers)))
+        runningCountAllYes += len([v for v in answers.values() if int(v) >= peopleInGroup])
+        peopleInGroup = 0
         answers = {}
+    else:
+        peopleInGroup += 1
+        for c in l.strip():
+            answers[c] = (answers.get(c) or 0) + 1
 
-    for c in l.strip():
-        #print("-" + c + '-')
-        answers[c] = 1
-
-#print('group count: {}'.format(len(answers)))
 runningCount += len(answers)
+runningCountAllYes += len([v for v in answers.values() if int(v) >= peopleInGroup])
 f.close()
-print('running count: {}'.format(runningCount))
+
+print('total running count: {}'.format(runningCount))
+print('all yes: {}'.format(runningCountAllYes))
